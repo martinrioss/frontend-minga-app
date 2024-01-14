ccbdf25206906dcf6ab779adb464deef93636bc5
Martin Rios <114596159+martinrioss@users.noreply.github.com>
2024-01-14 14:21:50 -0300

    Feat/layout (#2)
    * feat: navbar ready
    
    * feat: footer ready
    
    ---------
    
    Co-authored-by: martinrioss <martinrios.rr@gmail.com>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,46 @@
+import { Link } from 'react-router-dom'
+import footer from '../assets/footer.webp'
+import minga from '../assets/minga.jpg'
+import heart from '../assets/heart.svg'
+import vimeo from '../assets/Vimeo.svg'
+import facebook from '../assets/facebook-black.svg'
+import youtube from '../assets/Youtube.svg'
+import twitter from '../assets/Twitter.svg'
 export default function Footer () {
   return (
-    <footer>
-      <h1>FOOTER</h1>
+    <footer className='shadow-lg border-t'>
+      <img src={footer} alt="footer image" className='hidden md:block w-full h-40 object-center rounded-b-[100px]' />
+      <section className='flex flex-col md:flex-row items-center mt-5 mb-5 w-full'>
+        <div className='flex justify-evenly w-full md:w-[33%] m-3 font-semibold '>
+          <Link to={'/'}>Home</Link>
+          <Link to={'/mangas'}>Mangas</Link>
+        </div>
+        <div className='md:w-[33%] flex justify-center'>
+          <Link to={'/j'}>
+            <img src={minga} alt="logo minga" className='h-10 w-32 md:h-11 md:w-44' />
+          </Link>
+        </div>
+        <div className='mt-5 mb-5 md:w-[33%] md:flex md:flex-col md:items-center'>
+          <div className='flex justify-evenly md:w-[50%]'>
+            <a href="https://es-la.facebook.com" target="_blank" rel="noreferrer">
+              <img src={facebook} alt="facebook" />
+            </a>
+            <a href="https://twitter.com/" target="_blank" rel="noreferrer">
+              <img src={twitter} alt="twitter" />
+            </a>
+            <a href="https://vimeo.com/" target="_blank" rel="noreferrer">
+              <img src={vimeo} alt="vimeo" />
+            </a>
+            <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
+              <img src={youtube} alt="youtube" />
+            </a>
+          </div>
+          <Link className='mt-2 mb-2 flex items-center justify-center text-white bg-gradient-to-b from-gradient-start to-gradient-end w-32 h-10 rounded-lg md:w-44'>
+            <p>Donate</p>
+            <img src={heart} alt="heart" className='w-5 h-5 mt-1 ml-1'/>
+          </Link>
+        </div>
+      </section>
     </footer>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 export default {
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
   theme: {
     extend: {},
@@ -13,8 +14,12 @@ export default {
       gradient: {
         start: '#4338CA',
         end: '#5E52F3'
-      }
+      },
+      white: '#FFFFFF',
+      black: '#000000'
     }
   },
-  plugins: []
+  plugins: [
+    require('flowbite/plugin')
+  ]
 }
